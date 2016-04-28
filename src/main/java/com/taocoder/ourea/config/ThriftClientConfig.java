6225fb79692121a50cc6db0cbd5f58a921d67535
ketao <ketao>
2016-04-28 10:56:14 +0800

    config

---
+++ src/main/java/com/taocoder/ourea/config/ThriftClientConfig.java
@@ -0,0 +1,13 @@
+/*
+* Copyright (c) 2015 taocoder.com. All Rights Reserved.
+*/
+package com.taocoder.ourea.config;
+
+/**
+ * thrift client 相关配置
+ *
+ * @author tao.ke Date: 16/4/28 Time: 上午10:54
+ */
+public class ThriftClientConfig {
+
+}
---
+++ src/main/java/com/taocoder/ourea/config/ZkConfig.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2015 taocoder.com. All Rights Reserved.
+ */
+package com.taocoder.ourea.config;
+
+/**
+ * zk相关配置
+ * 
+ * @author tao.ke Date: 16/4/28 Time: 上午10:53
+ */
+public class ZkConfig {
+
+}
